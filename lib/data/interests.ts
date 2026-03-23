dc13a3e37462f160ab76b19896700813314d45cb
Adam Lowther <AdamKLowther@gmail.com>
2026-03-23 18:18:12 -0400

    feat: add quantified self

--- lib/data/interests.ts
+++ lib/data/interests.ts
@@ -11,4 +11,9 @@ export const interests: Interest[] = [
     description: 'Exploring sustainable energy solutions and their integration into modern infrastructure.',
     image: '/images/renewable_energy.png',
   },
+  {
+    name: 'Quantified Self',
+    description: 'Measuring and analyzing personal data to improve health and well-being. "Self-knowledge through numbers."',
+    image: 'https://quantifiedself.com/wp-content/themes/quantified-self/images/logo-qs-lt-blue.svg',
+  },
 ]
